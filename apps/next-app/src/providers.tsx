1964773074d0cc38cf77e60ee9a9e4a7cf4a227f
ofcljaved <javedofficialme@gmail.com>
2024-10-11 18:40:25 +0530

    chore: add theme in next

--- apps/next-app/src/providers.tsx
+++ apps/next-app/src/providers.tsx
@@ -2,16 +2,23 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RecoilRoot } from 'recoil'
 import { SessionProvider } from 'next-auth/react'
+import { NextThemeProvider } from '@repo/ui'
 const queryClient = new QueryClient()
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <SessionProvider>
-        <QueryClientProvider client={queryClient}>
-          <RecoilRoot>{children}</RecoilRoot>
-        </QueryClientProvider>
-      </SessionProvider>
-    </>
-  )
+    return (
+        <>
+            <SessionProvider>
+                <QueryClientProvider client={queryClient}>
+                    <NextThemeProvider 
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                        disableTransitionOnChange>
+                        <RecoilRoot>{children}</RecoilRoot>
+                    </NextThemeProvider>
+                </QueryClientProvider>
+            </SessionProvider>
+        </>
+    )
 }
 export default Providers
